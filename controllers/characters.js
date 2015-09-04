9a94515a9eb6ada95a31f065a765d6956c6267ed
James Traver <jtraverjr@gmail.com>
2015-09-04 10:41:07 -0500

    working restful mongo + express + node api

--- controllers/characters.js
+++ controllers/characters.js
@@ -18,12 +18,44 @@ controller.get('/', function(req, res, next) {
 });
 
 //get by id
+controller.get('/:id', function(req, res, next) {
+  charModel.findById(req.params.id, function(error, character) {
+    if (error) return error;
+    res.json(character);
+  });
+});
 
 //create
+controller.post('/', function(req, res, next) {
+  charModel.create(req.body, function(error, character) {
+    if (error) return error;
+    res.json(character);
+  });
+});
 
 //update by id
+controller.put('/:id', function(req, res, next) {
+  charModel.findByIdAndUpdate(req.params.id, req.body, function(error, character) {
+    if (error) return error;
+    res.json(character);
+  })
+});
+controller.patch('/:id', function(req, res, next) {
+  charModel.findByIdAndUpdate(req.params.id, req.body, function(error, character) {
+    if (error) return error;
+    res.json(character);
+  })
+});
 
 //delete by id
+controller.delete('/:id', function(req, res, next) {
+  charModel.findByIdAndRemove(req.params.id, req.body, function(error, character) {
+    if (error) return error;
+    res.json({
+      "Message": "User with the id of " + character.id + " has been removed"
+    });
+  });
+});
 
 
 module.exports = controller;
